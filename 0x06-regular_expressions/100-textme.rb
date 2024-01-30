be9ade2d33d571ba57e4f6bfa99115c4f4c02722
yomex96 <abayomirobertonawole2@gmail.com>
2024-01-29 23:48:31 -0800

    sent

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
+List = [Format[0].compact, Format[1].compact, Format[2].compact]
+puts List.join(',')
+
+#OR
+
+#puts ARGV[0]
+#  .scan(/(?<=from:|to:|flags:).*?(?=\])/)
+# .join(',')
