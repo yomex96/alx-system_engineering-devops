687a86d7a8f3408e225dcbb9b75486c6587eec98
yomex96 <abayomirobertonawole2@gmail.com>
2024-05-08 00:00:21 -0700

    kett

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-'''
-    this module contains the function top_ten
-'''
+"""request 10 most recent posts"""
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
-    try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+    request = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                           .format(subreddit),
+                           headers={"User-Agent": "holberton"},
+                           allow_redirects=False)
+
+    if request.status_code == 200:
+        for k in request.json().get("data").get("children"):
+            print(k.get("data").get("title"))
+    else:
         print(None)
-
-
-if __name__ == "__main__":
-    top_ten(argv[1])
