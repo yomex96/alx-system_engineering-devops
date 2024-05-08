71991c42a1b8573ea4aa523036974c3de09dc2b3
yomex96 <abayomirobertonawole2@gmail.com>
2024-05-08 00:37:38 -0700

    wise

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-"""request 10 most recent posts"""
+'''
+    Module contains top_ten function
+'''
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    request = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                           .format(subreddit),
-                           headers={"User-Agent": "holberton"},
-                           allow_redirects=False)
-
-    if request.status_code == 200:
-        for k in request.json().get("data").get("children"):
-            print(k.get("data").get("title"))
-    else:
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
         print(None)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
